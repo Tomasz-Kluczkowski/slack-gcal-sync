c661c7f35e2e1b3fa3b19a671cf7cb23c296364b
Tomasz Kluczkowski <tomaszk1@hotmail.co.uk>
2024-12-16 22:52:00 +0000

    feat(#8): implement google calendar integration
    add google calendar integration api and shape the very basics of error handling and configuration.
    add command line interface with clap crate

--- gcal_integration/src/lib.rs
+++ gcal_integration/src/lib.rs
@@ -1,8 +1,31 @@
-use configuration::{read_json_configuration, ConfigurationError};
-use google_calendar3::yup_oauth2::ServiceAccountKey;
+use google_calendar3::hyper_rustls::HttpsConnector;
+use google_calendar3::hyper_util::client::legacy::connect::HttpConnector;
+use google_calendar3::yup_oauth2::{ServiceAccountAuthenticator, ServiceAccountKey};
+use google_calendar3::{hyper_rustls, hyper_util, CalendarHub, Error as GoogleAPIError};
+use thiserror::Error;
 
-pub fn get_service_account_key(service_account_key_json_path: String) -> Result<ServiceAccountKey, ConfigurationError> {
-    Ok(read_json_configuration::<ServiceAccountKey>(
-        service_account_key_json_path,
-    )?)
+#[derive(Debug, Error)]
+pub enum GoogleCalendarIntegrationError {
+    #[error("Cannot communicate with Google Calendar Calendar API")]
+    GoogleCalendarCommunicationError(#[from] GoogleAPIError),
+
+    #[error("IO Error occurred")]
+    IOError(#[from] std::io::Error),
+}
+
+pub async fn get_calendar_hub(
+    service_account_key: ServiceAccountKey,
+) -> Result<CalendarHub<HttpsConnector<HttpConnector>>, GoogleCalendarIntegrationError> {
+    let service_account_authenticator = ServiceAccountAuthenticator::builder(service_account_key)
+        .build()
+        .await?;
+
+    let client = hyper_util::client::legacy::Client::builder(hyper_util::rt::TokioExecutor::new()).build(
+        hyper_rustls::HttpsConnectorBuilder::new()
+            .with_native_roots()?
+            .https_or_http()
+            .enable_http1()
+            .build(),
+    );
+    Ok(CalendarHub::new(client, service_account_authenticator))
 }
