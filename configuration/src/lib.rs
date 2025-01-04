0d67027477394275592f0e1512ce993175a3b260
Tomasz Kluczkowski <tomaszk1@hotmail.co.uk>
2025-01-04 21:02:20 +0000

    feat(issue-9): Add slack api integration
    Create new library for slack api integration code.
    Update dependencies and cargo.toml files.
    Remove unnecessary test annotation above mod tests import.
    Introduce rstest crate to use fixtures and parametrized tests.

--- configuration/src/lib.rs
+++ configuration/src/lib.rs
@@ -1,8 +1,7 @@
-#[cfg(test)]
 mod tests;
 
 use clap::Parser;
-use log::{info, warn};
+use log::info;
 use serde::de::DeserializeOwned;
 use serde::{Deserialize, Serialize};
 use std::fs::File;
@@ -10,6 +9,8 @@ use std::path::Path;
 use thiserror::Error;
 
 const DEFAULT_SERVICE_ACCOUNT_PATH: &str = ".secrets/.service_account.json";
+const DEFAULT_SLACK_USER_OAUTH_TOKEN_PATH: &str = ".secrets/.slack_user_oauth_token.json";
+const DEFAULT_LOGGING_CONFIG_PATH: &str = "config/logging_config.yaml";
 const DEFAULT_APPLICATION_CONFIG_PATH: &str = "config/application_config.json";
 const DEFAULT_CALENDAR_ID: &str = "primary";
 
@@ -26,18 +27,38 @@ pub enum ConfigurationError {
 pub struct ApplicationConfiguration {
     pub calendar_id: String,
     pub service_account_key_path: String,
+    pub slack_user_oauth_token_path: String,
+    pub logging_config_path: String,
 }
 
 #[derive(Parser, Debug)]
 #[command(version, about)]
 pub struct ApplicationCommandLineArguments {
-    #[arg(short, long)]
+    #[arg(
+        short,
+        long,
+        help = "The id of the calendar to synchronize. Usually your gmail email account."
+    )]
     pub calendar_id: Option<String>,
 
-    #[arg(short, long)]
+    #[arg(
+        short,
+        long,
+        help = "Path to the service account key json file from Google Cloud Project."
+    )]
     pub service_account_key_path: Option<String>,
 
-    #[arg(short, long, default_value = DEFAULT_APPLICATION_CONFIG_PATH)]
+    #[arg(short = 't', long, help = "Path to the slack user oauth token json file.")]
+    pub slack_user_oauth_token_path: Option<String>,
+
+    #[arg(
+        short,
+        long,
+        help = "Path to the log4rs logging configuration file. See https://docs.rs/log4rs/latest/log4rs/#configuration-via-a-yaml-file for possible options."
+    )]
+    pub logging_config_path: Option<String>,
+
+    #[arg(short, long, default_value = DEFAULT_APPLICATION_CONFIG_PATH, help = "Path to the application configuration file.")]
     pub application_config_path: String,
 }
 
@@ -62,21 +83,29 @@ pub fn get_application_configuration(
         );
         app_config
     } else {
-        warn!(
+        info!(
             "No application configuration file found at path: '{}'. Using default settings. \
          Make sure to provide configuration via command line args.",
             args.application_config_path
         );
         ApplicationConfiguration {
-            service_account_key_path: DEFAULT_SERVICE_ACCOUNT_PATH.to_string(),
             calendar_id: DEFAULT_CALENDAR_ID.to_string(),
+            service_account_key_path: DEFAULT_SERVICE_ACCOUNT_PATH.to_string(),
+            slack_user_oauth_token_path: DEFAULT_SLACK_USER_OAUTH_TOKEN_PATH.to_string(),
+            logging_config_path: DEFAULT_LOGGING_CONFIG_PATH.to_string(),
         }
     };
-    if args.calendar_id.is_some() {
-        app_config.calendar_id = args.calendar_id.unwrap().to_string();
+    if let Some(calendar_id) = args.calendar_id.as_ref() {
+        app_config.calendar_id = calendar_id.to_string();
     }
-    if args.service_account_key_path.is_some() {
-        app_config.service_account_key_path = args.service_account_key_path.unwrap().to_string();
+    if let Some(service_account_key_path) = args.service_account_key_path.as_ref() {
+        app_config.service_account_key_path = service_account_key_path.to_string();
+    }
+    if let Some(slack_user_oauth_token_path) = args.slack_user_oauth_token_path.as_ref() {
+        app_config.slack_user_oauth_token_path = slack_user_oauth_token_path.to_string();
+    }
+    if let Some(logging_config_path) = args.logging_config_path.as_ref() {
+        app_config.logging_config_path = logging_config_path.to_string();
     }
     Ok(app_config)
 }
--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,24 @@
+mod logging;
+
+use crate::logging::setup_default_logger;
 use anyhow::{Context, Result};
+use chrono::Utc;
 use clap::Parser;
 use configuration::{get_application_configuration, read_json_configuration, ApplicationCommandLineArguments};
 use gcal_integration::{get_calendar_events_for_today, get_calendar_hub};
 use google_calendar3::yup_oauth2::{ServiceAccountAuthenticator, ServiceAccountKey};
 use log::info;
-
-const LOGGING_CONFIG_PATH: &str = "config/logging_config.yaml";
+use log4rs::config::load_config_file;
+use reqwest::Client;
+use slack_integration::{
+    ProfileData, ProfileRequestBody, SlackApiClient, SLACK_API_BASE_URL, SLACK_USER_PROFILE_GET_ENDPOINT,
+    SLACK_USER_PROFILE_SET_ENDPOINT,
+};
+use std::env;
+use std::path::Path;
 
 async fn run() -> Result<()> {
-    log4rs::init_file(LOGGING_CONFIG_PATH, Default::default()).with_context(|| {
-        format!(
-            "Failed to initialize logging config at path: '{}'.",
-            LOGGING_CONFIG_PATH
-        )
-    })?;
+    let logging_handle = setup_default_logger();
 
     let args = ApplicationCommandLineArguments::parse();
     let application_config_path = args.application_config_path.clone();
@@ -25,6 +30,30 @@ async fn run() -> Result<()> {
         )
     })?;
 
+    if Path::new(application_configuration.logging_config_path.as_str()).exists() {
+        info!(
+            "Loading logging configuration from path: '{}'",
+            application_configuration.logging_config_path
+        );
+        let config_from_file = load_config_file(
+            application_configuration.logging_config_path.as_str(),
+            Default::default(),
+        )
+        .with_context(|| {
+            format!(
+                "Failed to load logging config at path: '{}'.",
+                application_configuration.logging_config_path
+            )
+        })?;
+        logging_handle.set_config(config_from_file);
+        info!("Successfully loaded logging configuration.");
+    } else {
+        info!(
+            "No logging configuration found at path: '{}'. Using default.",
+            application_configuration.logging_config_path
+        );
+    }
+
     let google_api_error_context = || {
         format!(
             "Failed to integrate with Google API using supplied service account key on path '{}'. \
@@ -33,7 +62,10 @@ async fn run() -> Result<()> {
         )
     };
 
-    info!("Reading google calendar service account key.");
+    info!(
+        "Reading google calendar service account key from path: '{}'.",
+        application_configuration.service_account_key_path
+    );
     let service_account_key =
         read_json_configuration::<ServiceAccountKey>(&application_configuration.service_account_key_path)
             .with_context(|| {
@@ -72,6 +104,31 @@ async fn run() -> Result<()> {
         println!("event end time: {:#?}", event.end);
         println!("\n");
     }
+    // Testing reading Slack user profile
+    // TODO: read this from a file same as service account key and override via env var. No command line arg.  Move this to get app configuration
+    let slack_user_auth_token = env::var("USER_AUTH_TOKEN").expect("USER_AUTH_TOKEN env variable not found.");
+    let slack_api_client = SlackApiClient::new(SLACK_API_BASE_URL.to_owned(), slack_user_auth_token, Client::new());
+    let slack_user_profile = slack_api_client
+        .get_user_profile(SLACK_USER_PROFILE_GET_ENDPOINT)
+        .await?;
+    println!("Previous Slack user profile: {:#?}", slack_user_profile);
+    println!("Status: {}", slack_user_profile.status);
+    println!("Url: {}", slack_user_profile.url);
+    println!("OK: {}", slack_user_profile.body.ok);
+    println!("Profile: {:?}", slack_user_profile.body.profile);
+
+    // Testing setting Slack user profile
+    let profile_data = ProfileData {
+        status_text: format!("testing rust messaging on: {}", Utc::now()),
+        status_emoji: ":mountain_railway:".to_string(),
+        status_expiration: 0,
+    };
+
+    let profile_request_body = ProfileRequestBody { profile: profile_data };
+    let slack_new_user_profile = slack_api_client
+        .set_user_profile(SLACK_USER_PROFILE_SET_ENDPOINT, &profile_request_body)
+        .await?;
+    println!("New Slack user profile: {:#?}", slack_new_user_profile);
 
     Ok(())
 }
